a047f6e1fced5267abc26dfb154c0100d509c1ba
ichchhanand <ichchhanandkasture@gmail.com>
2019-08-08 16:34:51 +0530

    my first commit

---
+++ src/com/Utility/java/AdditionOf2Numbers.java
@@ -0,0 +1,10 @@
+package com.Utility.java;
+
+public class AdditionOf2Numbers{
+	public int additionOfTwoNumbers(int firstNumber, int secondNumber)
+	{
+		int result= firstNumber + secondNumber;
+		return result;
+	}
+
+}
---
+++ src/com/Utility/java/AdditionOfTwoNumbers.java
@@ -0,0 +1,16 @@
+package com.Utility.java;
+
+import java.util.Scanner;
+
+public class AdditionOfTwoNumbers {
+	int firstNumber, seccondNumber,result;
+	Scanner scanner=new Scanner(System.in);
+	System.out.println("Enter the first number");
+	firstNumber=scanner.nextInt();
+	
+	System.out.println("Enter the second number");
+	secondNumber=scanner.nextInt();
+	result=additionOfTwoNumbers(firstNumber, secondNumber);
+	
+
+}
